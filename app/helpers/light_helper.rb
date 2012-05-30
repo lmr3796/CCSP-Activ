06a5e09328279be01a39d6ee74757cc950334ebf
lmr3796 <lmr3796@lmr3796-U36JC.(none)>
2012-05-30 21:01:39 +0800

    Helpers for music and script

--- app/helpers/light_helper.rb
+++ app/helpers/light_helper.rb
@@ -1,2 +1,14 @@
 module LightHelper
+  def music_mp3(activity_id=nil)
+    return "https://dl.dropbox.com/u/31496039/OLD0307.mp3"
+  end
+  def music_oga(activity_id=nil)
+    return "https://dl.dropbox.com/u/31496039/OLD0307.ogg"
+  end
+  def music_m4a(activity_id=nil)
+    return "https://dl.dropbox.com/u/31496039/OLD0307.m4a"
+  end
+  def light_script(activity_id=nil)
+    return "/old.json"
+  end
 end
