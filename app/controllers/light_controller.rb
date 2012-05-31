9c6d02cc74c306f53e05cc2b726e3bcfad829ffb
lmr3796 <lmr3796@lmr3796-U36JC.(none)>
2012-06-01 01:46:06 +0800

    Remove light helpers and wrap up db finding

--- app/controllers/light_controller.rb
+++ app/controllers/light_controller.rb
@@ -1,11 +1,19 @@
 require 'json'
 class LightController < ApplicationController
+  def find_music_rec(p)
+	return Activity.find(p[:act_id])
+  end
+  def param_valid(p)
+      return !p[:act_id].blank?
+  end
   def show
-    id=params[:act_id]
-    @script = JSON(ApplicationController.helpers.light_script(id))
-    @mp3 = ApplicationController.helpers.music_mp3(id)
-    @oga = ApplicationController.helpers.music_oga(id)
-    @m4a = ApplicationController.helpers.music_m4a(id)
+    if param_valid(params)
+      id=params[:act_id]
+      @script = JSON(find_music_rec(params).light_json)
+      @mp3 = find_music_rec(params).music_url
+      @oga = nil        #find_music_rec(params).music_oga
+      @m4a = nil        #find_music_rec(params).music_m4a
+    end
     render :layout => false
   end
 
--- app/helpers/light_helper.rb
+++ app/helpers/light_helper.rb
@@ -1,18 +1,2 @@
 module LightHelper
-  def music_mp3(activity_id=nil)
-	@a = Activity.find(activity_id)
-    #return "https://dl.dropbox.com/u/31496039/OLD0307.mp3"
-	return @a.music_url
-  end
-  def music_oga(activity_id=nil)
-    return "https://dl.dropbox.com/u/31496039/OLD0307.ogg"
-  end
-  def music_m4a(activity_id=nil)
-    return "https://dl.dropbox.com/u/31496039/OLD0307.m4a"
-  end
-  def light_script(activity_id=nil)
-	@a = Activity.find(activity_id)
-	return @a.light_json
-    #return "/old.json"
-  end
 end
