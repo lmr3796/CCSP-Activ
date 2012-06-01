ea96cc7a6e270c65fb848616f851a0a464ecb3ce
lmr3796 <lmr3796@lmr3796-U36JC.(none)>
2012-06-01 14:38:52 +0800

    Move purplebleed's js into a single bubble_timeline.js.erb

--- app/controllers/light_controller.rb
+++ app/controllers/light_controller.rb
@@ -7,17 +7,22 @@ class LightController < ApplicationController
       return !p[:act_id].blank?
   end
   def show
+    @script =[] 
     if param_valid(params)
-      id=params[:act_id]
       @script = JSON(find_music_rec(params).light_json)
+      if @script.blank?
+          @script = []
+      end
       @mp3 = find_music_rec(params).music_url
-      @oga = nil        #find_music_rec(params).music_oga
-      @m4a = nil        #find_music_rec(params).music_m4a
+      @oga = ""        #find_music_rec(params).music_oga
+      @m4a = ""        #find_music_rec(params).music_m4a
+    else
     end
     render :layout => false
   end
 
-  def update
-    redirect_to :show
+  def new 
+    #redirect_to :show
+    @script = LightScript.new
   end
 end
