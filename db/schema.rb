e7a631a24cb1ef7214fa01e6a8bd6ada4085ddc6
lmr3796 <lmr3796@lmr3796-U36JC.(none)>
2012-06-02 02:06:22 +0800

    Removed light_json field from activity

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120531180453) do
+ActiveRecord::Schema.define(:version => 20120601180451) do
 
   create_table "activities", :force => true do |t|
     t.string   "act_name"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(:version => 20120531180453) do
     t.text     "act_description"
     t.text     "act_image_url"
     t.text     "music_url"
-    t.text     "light_json"
   end
 
   create_table "departments", :force => true do |t|
